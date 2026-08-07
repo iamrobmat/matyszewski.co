c4574fdeefcb56e6f47202cf30b1dede60086fc1
iamrobmat <robert.matyszewski@gmail.com>
2026-08-07 12:20:42 +0200

    Dodanie podstrony do umawiania rozmów
    ## Nowe pliki:
    - "umow-rozmowe/index.html" - Podstrona z informacją o Robercie oraz osadzonym wyborem dwóch spotkań z Cal.com.
    - "umow-rozmowe/umow-rozmowe.css" - Responsywny układ informacji i kalendarza dla desktopu oraz telefonu.
    
    ## Zmodyfikowane:
    - "index.html" - Przyciski umawiania rozmowy prowadzą do nowej podstrony.
    - "uslugi/index.html" - Linki do rezerwacji prowadzą do nowej podstrony.
    - "scripts/build-site.sh" - Podstrona rezerwacji trafia do artefaktu GitHub Pages.
    
    ## Usunięte:
    - Brak.
    
    Statystyki: +2 nowych, ~3 zmodyfikowanych, -0 usuniętych

--- scripts/build-site.sh
+++ scripts/build-site.sh
@@ -15,6 +15,7 @@ mkdir -p "$output_dir"
 cp index.html styles.css analytics.js analytics-config.js CNAME .nojekyll "$output_dir/"
 cp -R blog "$output_dir/blog"
 cp -R uslugi "$output_dir/uslugi"
+cp -R umow-rozmowe "$output_dir/umow-rozmowe"
 
 node scripts/configure-analytics.mjs "$output_dir/analytics-config.js"
 
